32029de58468dc40527d330cae1704d5fd32dd32
SpamMasubi <38049610+SpamMasubi@users.noreply.github.com>
2022-07-25 14:39:36 -0700

    change of speed

--- Tetris.py
+++ Tetris.py
@@ -317,7 +317,7 @@ def main(win):
       next_piece = get_shape()
       clock = pygame.time.Clock()
       fall_time = 0
-      fall_speed = 0.27
+      fall_speed = 0.7
       level_time = 0
       score = 0
 
